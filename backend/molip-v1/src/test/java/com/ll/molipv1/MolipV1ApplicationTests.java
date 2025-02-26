47e05a50485a9e7f357203f2833ed2bb414a127e
sik2 <bickwang123@gmail.com>
2025-02-26 14:52:30 +0900

    init

---
+++ backend/molip-v1/src/test/java/com/ll/molipv1/MolipV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.molipv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolipV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
